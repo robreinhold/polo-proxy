a992f4cadbf8513de86f084276a6247827d69310
Rob Reinhold <rreinhold@expedia.com>
2016-02-25 13:39:23 -0600

    Netty-Router - basic text version works, moving to class targets

--- src/main/java/com/expedia/poloproxy/NettyRouterStarter.java
+++ src/main/java/com/expedia/poloproxy/NettyRouterStarter.java
@@ -1,21 +1,19 @@
 package com.expedia.poloproxy;
 
-import com.oracle.tools.packager.Log;
-import io.netty.handler.codec.http.*;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.handler.codec.http.router.Router;
-import org.littleshoot.proxy.*;
-import org.littleshoot.proxy.extras.SelfSignedMitmManager;
-import org.littleshoot.proxy.impl.DefaultHttpProxyServer;
 
-import java.net.InetSocketAddress;
-import java.net.UnknownHostException;
 import java.util.logging.Logger;
 
 @SuppressWarnings({"PMD.UseUtilityClass"})
 public class NettyRouterStarter {
 
     private static final Logger LOG = Logger.getLogger(NettyRouterStarter.class.getName());
-    private static final String HOST = HttpHeaders.Names.HOST;
+    public static final int PORT = 5050;
 
 
     public static void main(String[] args) throws Exception {
@@ -23,11 +21,29 @@ public class NettyRouterStarter {
 
         Router<String> router = new Router<String>()
                 .GET("/",             "Index page")
-                .GET("/articles/:id", "Article show page")
+                .GET("/consul/:serviceName", "Article show page")
                 .notFound("404 Not Found");
-        Log.info(router.toString());
+        LOG.info(router.toString());
 
-        
+        NioEventLoopGroup bossGroup   = new NioEventLoopGroup(1);
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+                    .childOption(ChannelOption.TCP_NODELAY, java.lang.Boolean.TRUE)
+                    .childOption(ChannelOption.SO_KEEPALIVE, java.lang.Boolean.TRUE)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new HttpRouterServerInitializer(router));
+
+            Channel ch = b.bind(PORT).sync().channel();
+            System.out.println("Server started: http://127.0.0.1:" + PORT + '/');
+
+            ch.closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
     }
 }
 
